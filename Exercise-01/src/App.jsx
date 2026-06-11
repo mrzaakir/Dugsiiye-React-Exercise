a6ea281a8a99cba851499daca654e374ccb68bc7
ZaCky TaAni <mrzaa@example.com>
2026-06-11 13:49:15 +0300

    Exercise-01 New Component

---
+++ Exercise-01/src/App.jsx
@@ -0,0 +1,12 @@
+import UserCard from "./UserCard";
+
+const App = () =>{
+    return(
+        <>
+            <h1>Exercise 01</h1>
+            < UserCard />
+
+        </>
+    )
+}
+export default App;
---
+++ Exercise-01/src/UserCard.jsx
@@ -0,0 +1,16 @@
+const UserCard = () =>{
+    return(
+        <>
+            <h2>UserName: ZaCky</h2>
+            <span>Age: 25</span>
+            <h3>Location: Ireland</h3>
+
+            <div>
+                <p><a href="#">ZaCky@gmail.com</a></p>
+            </div>
+
+        </>
+    )
+}
+
+export default UserCard;
