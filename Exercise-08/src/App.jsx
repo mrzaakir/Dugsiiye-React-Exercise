864cab411891b66a3dad3c93124a6d52265558f4
mrzaakir <mrzaakiraj@gmail.com>
2026-06-18 14:45:13 +0300

    TimerCounter

---
+++ Exercise-08/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import CountDown from './CountDown'
+
+
+function App() {
+  
+
+  return (
+    
+    <>
+        <CountDown/>
+    </>
+
+  )
+}
+
+export default App
