cd2defdaa7d8adc8c668d7cb358f0148ed841225
mrzaakir <mrzaakiraj@gmail.com>
2026-06-18 20:36:36 +0300

    GitHubSearchInfo

---
+++ Exercise-09/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import GitHubUserSearch from './GitHubUserSearch'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <GitHubUserSearch/>
+    </>
+  )
+}
+
+export default App
