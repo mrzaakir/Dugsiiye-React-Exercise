8ebbf47992105d0d1b55d0a6a2c9191e1793d2a2
mrzaakir <mrzaakiraj@gmail.com>
2026-08-01 19:06:45 +0300

    Feat: added StudentRegistrationForm

---
+++ Exercise-20/src/App.jsx
@@ -0,0 +1,16 @@
+import StudentRegistration from "./StudentRegistration"
+// import React from 'react';
+// import { useState, useEffect } from 'react';
+import './index.css'
+function App() {
+  
+
+  return (
+    <>
+     <StudentRegistration/>
+
+    </>
+  )
+}
+
+export default App
